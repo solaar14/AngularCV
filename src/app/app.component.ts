13e46b23ae0321732ed58fb76f56e8ec42348e04
solaar14 <eric.creahi@gmail.com>
2018-09-13 09:38:15 +0000

    ajouter l'attribut tile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-
 import { Component } from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`  mat-grid-tile {
+              background: lightblue;
+          }`]
 })
 export class AppComponent {
-  title = 'AngularCV';
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+ 
+  ];
 }
